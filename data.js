4ac8e72d4f1c6a02a196750e2a56f98ee17fa246
Alifbhai19 <alifbhai909@gmail.com>
2026-08-16 16:21:49 +0600

    Add DAYS array and populate HTML elements

---
+++ data.js
@@ -0,0 +1,18 @@
+const DAYS=[
+
+{
+day:1,
+bio:"কোষ ও এর গঠন",
+phy:"ভৌত রাশি",
+chem:"পদার্থের গঠন",
+math:"সেট ও ফাংশন"
+}
+
+];
+
+const d=DAYS[0];
+
+document.getElementById("bio").innerText=d.bio;
+document.getElementById("phy").innerText=d.phy;
+document.getElementById("chem").innerText=d.chem;
+document.getElementById("math").innerText=d.math;
